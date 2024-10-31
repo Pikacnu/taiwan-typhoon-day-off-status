3d8b6cae46be5d54f7b1bf668f2afe17def03f25
Pikacnu <92923578+Pikacnu@users.noreply.github.com>
2024-10-31 21:53:51 +0800

    feat: add embed generater

---
+++ test.ts
@@ -0,0 +1,29 @@
+import puppeteer from 'puppeteer';
+import fs from 'fs/promises';
+import { readFile } from 'fs/promises';
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export async function renderMapImage(): Promise<string> {
+	// Launch a headless browser
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+	page.setViewport({ width: 1344, height: 1080 });
+
+	// Set up the HTML and CSS needed for Leaflet
+	await page.setContent(await readFile('./rawhtml/screenshot.html', 'utf-8'));
+
+	// Wait for the map to load
+	await page.waitForResponse('http://localhost:3000/api/typhoon');
+
+	// Take a screenshot and save it as a base64 string
+	const imageBuffer = await page.screenshot({ fullPage: false });
+	await browser.close();
+	// Save the image to a file
+	const imagePath = './map.png';
+	await fs.writeFile(imagePath, imageBuffer);
+	return imagePath;
+}
+
+renderMapImage();
